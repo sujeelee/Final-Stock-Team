bb4e14efb7eb312473918f4667520c086c79a500
sujeelee <sujee54@gmail.com>
2024-09-20 16:26:43 +0900

    Update HomeController.java

--- src/main/java/kh/st/spring/controller/HomeController.java
+++ src/main/java/kh/st/spring/controller/HomeController.java
@@ -1,16 +1,22 @@
 package kh.st.spring.controller;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kh.st.spring.dao.PostDAO;
 
 @Controller
 public class HomeController {
 	
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Local local, Model model) {
+		
+		System.out.println();
 		return "/home";//타일즈에서 /*로 했기 때문에 /를 붙임
 	}
 	
