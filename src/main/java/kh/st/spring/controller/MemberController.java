b27dfcf6908fd8d942450a611c457dcc506f2167
cocoparoma <bnbz201@naver.com>
2024-09-20 15:04:10 +0900

    로그인 테스트

--- src/main/java/kh/st/spring/controller/MemberController.java
+++ src/main/java/kh/st/spring/controller/MemberController.java
@@ -2,7 +2,11 @@ package kh.st.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import kh.st.spring.model.dto.LoginDTO;
 import kh.st.spring.service.MemberService;
 
 @Controller
@@ -10,4 +14,16 @@ public class MemberController {
     
     @Autowired
     private MemberService memberService;
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "/member/login";
+    }
+
+    @PostMapping("/login")
+    public String login_post(Model mo, LoginDTO user_){
+
+        return "/member/login";
+    }
 }
